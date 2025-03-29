5f5aedbc6fbb0c0833ffcff731d45a9cf17e8d5a
Jaren Ramos <jarenramos234@gmail.com>
2025-03-29 14:24:49 -0500

    vist de registro agregada

--- app1/urls.py
+++ app1/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     #path('', views.home, name='home'),
-    #path('private_home/', views.private_home, name="private_home"),
+    path('private_home/', views.private_home, name="private_home"),
     path('registro/', views.registrar_usuario, name="registrar_usuario"),
 ]
--- app1/views.py
+++ app1/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 from django.contrib.messages import success
 from django.http import HttpResponse
 
@@ -10,3 +11,20 @@ def registrar_usuario(request):
     if request.method == "GET":
         form = UserCreationForm()
         return render(request, 'app1/login.html', {'form': form})
+    else:
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username_cleaned = form.cleaned_data["username"]
+            password_cleaned = form.cleaned_data["password1"]
+            user = authenticate(username=username_cleaned, password=password_cleaned)
+            if user is not None:
+                login(request, user)
+                return redirect('private_home')
+
+        success(request, "Vuelve a intentarlo!")
+        return render(request, 'app1/login.html', {'form': form})
+
+
+def private_home(request):
+    return HttpResponse(f"Este es el panel privado: Bienvenido {request.user}")
