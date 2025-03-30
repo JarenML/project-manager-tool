ae5939f9839d052a853fb50abf6daff94700c984
Jaren Ramos <jarenramos234@gmail.com>
2025-03-29 22:50:28 -0500

    Customizando el formulario y renombrando a UserRegister

--- app1/views.py
+++ app1/views.py
@@ -2,28 +2,36 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from django.contrib.messages import success
+from django.contrib import messages
 from django.http import HttpResponse
-
+from .forms import UserRegister
+from django.contrib.auth.models import User
 
 # Create your views here.
 def registrar_usuario(request):
     if request.method == "GET":
-        form = UserCreationForm()
+        form = UserRegister()
         return render(request, 'app1/login.html', {'form': form})
     else:
-        form = UserCreationForm(request.POST)
+        form = UserRegister(request.POST)
         if form.is_valid():
-            form.save()
             username_cleaned = form.cleaned_data["username"]
-            password_cleaned = form.cleaned_data["password1"]
-            user = authenticate(username=username_cleaned, password=password_cleaned)
-            if user is not None:
-                login(request, user)
-                return redirect('private_home')
+            password1_cleaned = form.cleaned_data["password1"]
+            password2_cleaned = form.cleaned_data["password2"]
 
-        success(request, "Vuelve a intentarlo!")
+            if password1_cleaned != password2_cleaned:
+                messages.success(request, "Las contraseñas no coinciden")
+                return redirect('registrar_usuario')
+
+            user = User.objects.create_user(username=username_cleaned, password=password1_cleaned)
+            user.save()
+            login(request, user)
+            return redirect('private_home')
+
         return render(request, 'app1/login.html', {'form': form})
+
+
+
 
 
 def private_home(request):
